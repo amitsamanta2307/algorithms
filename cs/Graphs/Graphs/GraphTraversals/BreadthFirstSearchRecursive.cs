69c3c6d0e563d34a7cbb044f9b52f8be22be4fa9
Amit Samanta <developer.amitsamanta@gmail.com>
2020-10-13 01:06:50 +0530

    add the graph traversal method for breadth first search recursive

---
+++ cs/Graphs/Graphs/GraphTraversals/BreadthFirstSearchRecursive.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.GraphTraversals
+{
+    public class BreadthFirstSearchRecursive
+    {
+        public const int DEPTH_TOKEN = -1;
+
+        public int BFS(IList<IList<int>> graph, int start, int count)
+        {
+            bool[] visited = new bool[count];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            queue.Enqueue(DEPTH_TOKEN);
+            return BFS(visited, queue, graph);
+        }
+
+        private int BFS(bool[] visited, Queue<int> queue, IList<IList<int>> graph)
+        {
+            int current = queue.Dequeue();
+
+            if (current == DEPTH_TOKEN)
+            {
+                queue.Enqueue(DEPTH_TOKEN);
+                return 1;
+            }
+
+            // Node is visited
+            if (visited[current]) 
+                return 0;
+
+            // Visit this node.
+            visited[current] = true;
+
+            // Add all neighbors to queue.
+            IList<int> neighbors = graph[current];
+            if (neighbors != null)
+            {
+                foreach (var next in neighbors)
+                    if (!visited[next])
+                        queue.Enqueue(next);
+            }
+
+            int depth = 0;
+
+            while (true)
+            {
+                if (queue.Count == 1 && queue.Peek() == DEPTH_TOKEN)
+                    break;
+
+                // The depth is the sum of all DEPTH_TOKENS encountered.
+                depth += BFS(visited, queue, graph);
+            }
+
+            return depth;
+        }
+    }
+}
---
+++ cs/Graphs/Graphs/Program.cs
@@ -0,0 +1,41 @@
+﻿using Graphs.GraphTraversals;
+using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    class Program
+    {
+        public static void AddUndirectedEdge(IList<IList<int>> graph, int from, int to)
+        {
+            graph[from].Add(to);
+            graph[to].Add(from);
+        }
+        static void Main(string[] args)
+        {
+            // Create a Graph
+            int count = 9;
+
+            IList<IList<int>> graph = new List<IList<int>>();
+
+            for (int i = 0; i < count; i++)
+                graph.Add(new List<int>());
+
+            // Add the undirected Edges
+            AddUndirectedEdge(graph, 0, 1);
+            AddUndirectedEdge(graph, 0, 8);
+            AddUndirectedEdge(graph, 0, 3);
+            AddUndirectedEdge(graph, 1, 7);
+            AddUndirectedEdge(graph, 7, 2);
+            AddUndirectedEdge(graph, 2, 3);
+            AddUndirectedEdge(graph, 2, 5);
+            AddUndirectedEdge(graph, 5, 6);
+            AddUndirectedEdge(graph, 3, 4);
+            AddUndirectedEdge(graph, 4, 8);
+
+            BreadthFirstSearchRecursive ojb1 = new BreadthFirstSearchRecursive();
+
+            Console.WriteLine("BFS depth: {0}", ojb1.BFS(graph, 0, 9));
+        }
+    }
+}
